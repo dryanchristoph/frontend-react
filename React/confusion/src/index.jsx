6cee049dce380942355cf134562e1a1d8b539878
dryanchristoph <christophorusdryantoro@gmail.com>
2020-12-23 10:44:53 +0700

    update frontend

--- React/confusion/src/index.jsx
+++ React/confusion/src/index.jsx
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
 import './App.css';
+import './Posts.css';
 import './index.css';
 import { store } from './helpers';
 import { App } from './App';
--- React/confusion/src/pages/HomePage/HomePage.js
+++ React/confusion/src/pages/HomePage/HomePage.js
@@ -38,24 +38,84 @@ class HomePage extends Component {
       const {items } = this.state;
         return (
           <div className="container">
+            <div class="pull-left">
             <h1>Employee Activities</h1>
+            <hr />
             {
               items ? items.map(item => {
               //const {_id, title, description, createdAt, status, username, firstname, lastname} = item;
                return (
- 
-               <div key={item._id} className="bgCircle">
-                <center><img /* src={thumbnail} */ alt={item.user.firstName} className="circle"/> </center><br />
-                <div className="ctr">
-                    <b>{item.title}</b><br />
-                    {item.description}<br />
-                    by {item.user.firstname} {item.user.lastname}
-                    </div>
-    
+                <div class="panel panel-default row">
+                  <div class="panel-body">
+                  <section class="post-heading">
+                    <div class="row">
+                        <div class="col-md-11">
+                            <div class="media">
+                            <div class="media-left">
+                              <a href="#">
+                                  <img class="media-object photo-profile" src="http://0.gravatar.com/avatar/38d618563e55e6082adf4c8f8c13f3e4?s=40&d=mm&r=g" width="40" height="40" alt="..." />
+                                </a>
+                              </div>
+                              <div class="media-body">
+                                <a href="#" class="anchor-username"><h4 class="media-heading">{item.user.firstname} {item.user.lastname}</h4></a> 
+                                <a href="#" class="anchor-time">51 mins</a>
+                              </div>
+                            </div>
+                        </div>
+                         <div class="col-md-1">
+                             <a href="#"><i class="glyphicon glyphicon-chevron-down"></i></a>
+                         </div>
+                    </div>             
+               </section>
+               <section class="post-body">
+                 <p><b>{item.title}</b></p>
+                   <p>{item.description}.</p>
+               </section>
+               <section class="post-footer">
+                   <hr />
+                   <div class="post-footer-option container">
+                        <ul class="list-unstyled">
+                            <li><a href="#"><i class="glyphicon glyphicon-thumbs-up"></i> Like</a></li>
+                            <li><a href="#"><i class="glyphicon glyphicon-comment"></i> Comment</a></li>
+                            <li><a href="#"><i class="glyphicon glyphicon-share-alt"></i> Share</a></li>
+                        </ul>
+                   </div>
+                   <div class="post-footer-comment-wrapper">
+                       <div class="comment-form">
+                           
+                       </div>
+                       <div class="comment">
+                            <div class="media">
+                              {/* <div class="media-left">
+                                <a href="#">
+                                  <img class="media-object photo-profile" src="http://0.gravatar.com/avatar/38d618563e55e6082adf4c8f8c13f3e4?s=40&d=mm&r=g" width="32" height="32" alt="..." />
+                                </a>
+                              </div>
+                              <div class="media-body">
+                                <a href="#" class="anchor-username"><h4 class="media-heading">Media heading</h4></a> 
+                                <a href="#" class="anchor-time">51 mins</a>
+                              </div> */}
+                            </div>
+                       </div>
+                   </div>
+               </section>
+                  
+  
+                  {/* <div key={item._id} className="bgCircle">
+                    <center><img src={thumbnail} alt={item.user.firstName} className="circle"/> </center><br />
+                    <div className="ctr">
+                        <b>{item.title}</b><br />
+                        {item.description}<br />
+                        by {item.user.firstname} {item.user.lastname}
+                        </div>
+        
+                    </div> */}
+                  </div>
                 </div>
               );
             }) : null
           }
+          </div>
           </div>
         );
  
